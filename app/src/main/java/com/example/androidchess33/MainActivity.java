8efa79c27ff0775f2816f7f06988d57542d4cd77
Jawad Mamun <jawadmamun@gmail.com>
2021-05-02 23:33:56 -0400

    Added Initial Chessboard Layout

--- app/src/main/java/com/example/androidchess33/MainActivity.java
+++ app/src/main/java/com/example/androidchess33/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.androidchess33;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
